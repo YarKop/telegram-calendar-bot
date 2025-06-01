f3003ea3387b762f4072549cf55a1770a3e83459
YarKop <48286742+YarKop@users.noreply.github.com>
2025-06-01 00:30:26 -0400

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -123,11 +123,11 @@ async def main():
 
 if __name__ == '__main__':
     try:
-        asyncio.get_event_loop().run_until_complete(main())
+        asyncio.run(main())
     except RuntimeError as e:
-        if str(e).startswith('This event loop is already running'):
-            loop = asyncio.get_event_loop()
-            loop.create_task(main())
-            loop.run_forever()
+        if "already running" in str(e):
+            import nest_asyncio
+            nest_asyncio.apply()
+            asyncio.get_event_loop().create_task(main())
         else:
             raise
